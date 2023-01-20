6615cad874ffa12e8296b7a17dca7ad5eadaecc9
helena-balabin <helena.balabin@kuleuven.be>
2023-01-20 11:41:09 +0100

    Add a simple setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='skip-thoughts',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
